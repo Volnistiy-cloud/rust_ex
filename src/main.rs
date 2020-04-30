308a986e1043acf42f9cdaceff0fa6eb421fb8ce
Dima <di.vo.se@mail.ru>
2020-04-30 20:41:40 +0300

    improve alghoritm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use colored::Colorize;
 use std::io;
 
+
 fn main() {
    let mut vector= Vec::new();
    loop {
@@ -18,38 +19,38 @@ fn main() {
         
     };
 
-    for count in 1..number {
+    for count in 2..number {
         let mut _done = true;
-        if count % 2 != 0 {
+       // if count % 2 != 0 {
             
             if _done {
                 let stop = ((count as f64).sqrt() + 1.0) as i64;
-                    for i in 3..stop {
-                        if i % 2 != 0 {
+                    for i in 2..stop {
+                       // if i % 2 != 0 {
                             if count % i == 0 {
                                 _done = false;
                                 break;
                             }
-                
-                        }
+                       // }
                     }
                 }
                 if _done {
                    //print!("{} ", count);
                    vector.push(count);
                 }
-            }
+          //  }
         }
-        let mut i = 1;
-        for  mut i in i..vector.len(){
+   
+        for i in 1..vector.len(){
            
-        if vector[i] - vector[i - 1] == 2{
-            print!("{}, {}, ", vector[i - 1].to_string().green(), vector[i].to_string().green());
-           i = i + 1;
-           }
-           else {
+        //if vector[i] - vector[i - 1] == 2{
+        //    print!("{}, {}, ", vector[i - 1].to_string().green(), vector[i].to_string().green());
+          //continue;
+          // }
            print!("{}, ", vector[i - 1]);
-           }
+       
+       
         }
+        //print!("{}, ", vector[i - 1]);
 break;} 
 }     
